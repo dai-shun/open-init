08227117140352964cfe2072b805dade3a88ea8d
daishun <shun.dai@zhaopin.com.cn>
2016-09-19 16:15:06 +0800

    nomal

--- bin/open-init.js
+++ bin/open-init.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 var vfs = require('vinyl-fs');
 var fs = require('fs');
 var through = require('through2');
@@ -14,32 +13,22 @@ if (process.argv.length === 3 &&
     console.log(require('../package').version);
     return;
 }
+var cwd;
 if(process.argv[2] === '--nomal'){
-    var cwd = join(__dirname, '../demo/nomal');
-    var dest = process.cwd();
-
-    vfs.src(['**/*', '!node_modules/**/*'], {cwd: cwd, cwdbase: true, dot: true})
-        .pipe(template(dest))
-        .pipe(vfs.dest(dest))
-        .on('end', function() {
-            // fs.renameSync(path.join(dest,'gitignore'),path.join(dest,'.gitignore'));
-            require('../lib/install');
-        })
-        .resume();
+    cwd = join(__dirname, '../demo/nomal');
+}else if (process.argv[2] === '--antd') {
+    cwd = join(__dirname, '../demo');
 }
-if (process.argv[2] === '--antd') {
-    var cwd = join(__dirname, '../demo');
-    var dest = process.cwd();
+var dest = process.cwd();
 
-    vfs.src(['**/*', '!node_modules/**/*'], {cwd: cwd, cwdbase: true, dot: true})
-        .pipe(template(dest))
-        .pipe(vfs.dest(dest))
-        .on('end', function() {
-            // fs.renameSync(path.join(dest,'gitignore'),path.join(dest,'.gitignore'));
-            require('../lib/install');
-        })
-        .resume();
-}
+vfs.src(['**/*', '!node_modules/**/*'], {cwd: cwd, cwdbase: true, dot: true})
+    .pipe(template(dest))
+    .pipe(vfs.dest(dest))
+    .on('end', function() {
+        fs.renameSync(path.join(dest,'gitignore'),path.join(dest,'.gitignore'));
+        require('../lib/install');
+    })
+    .resume();
 function template(dest) {
     return through.obj(function (file, enc, cb) {
         if (!file.stat.isFile()) {
